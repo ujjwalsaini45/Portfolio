f752e24af64f31342919bbb86bbedf2ec4c87ce1
ujjwalsaini45 <ujjwalsaini00@gmail.com>
2026-05-03 19:33:13 +0530

    fix: ignore eslint during build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,16 @@
 import { defineConfig } from "eslint/config";
 
-const eslintConfig = defineConfig([]);
+const eslintConfig = defineConfig([
+  {
+    rules: {},
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+]);
 
 export default eslintConfig;
